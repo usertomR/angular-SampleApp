0c3c28358ee213e5e410ac6f0b5ecbec50e8a413
usertomR <87595304+usertomR@users.noreply.github.com>
2022-10-29 18:05:46 +0900

    learned the example to use @output

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,18 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent {
+  @Input() product!: Product | undefined;
+  // ProductAlertsComponentが、notifyプロパティの値が変更されたときにイベントを発行することができる。
+  @Output() notify = new EventEmitter();
 
-  @Input() product!: Product;
-  
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
