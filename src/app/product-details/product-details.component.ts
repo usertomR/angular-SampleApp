1afd409d5d0aaa0c69d0808888a58fd7a8f5b3b8
usertomR <87595304+usertomR@users.noreply.github.com>
2022-10-30 11:18:18 +0900

    updated a product-details component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+// productsの配列をインポート
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  // どうやらユニオン型らしい
+  product: Product | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    // routeParamsの中身の表示。ex: {"params":{"productId":"1"}}
+    window.alert(JSON.stringify(routeParams))
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
